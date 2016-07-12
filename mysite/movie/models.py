16c313017bd8cf1f6a8a86ea3514b0da7e713e2c
eryk <me@eryk.io>
2016-07-12 13:22:19 +0200

    changed css, added models and included settings

--- mysite/movie/models.py
+++ mysite/movie/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=30)
+
+class Director(models.Model):
+    name = models.CharField(max_length=150)
+
+class Type(models.Model):
+    name = models.CharField(max_length=30)
+
+class Entry(models.Model):
+    genre = models.ManyToManyField(Genre)
+    director = models.ManyToManyField(Director)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    const = models.CharField(max_length=30)
+    name = models.CharField(max_length=150)
+    rate = models.CharField(max_length=30)
+    rate_imdb = models.CharField(max_length=30)
+    runtime = models.CharField(max_length=30)
+    year = models.CharField(max_length=30)
+    votes = models.CharField(max_length=30)
