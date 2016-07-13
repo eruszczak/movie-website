76fac44dd473020406cd55eb058f8d829c6e366f
eryk <kasumierka@gmail.com>
2016-07-13 12:04:49 +0200

    changes to navbar, added about page

--- mysite/movie/urls.py
+++ mysite/movie/urls.py
@@ -3,8 +3,11 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.index, name='home'),
+    url(r'^about/$', views.about, name='about'),
+
     url(r'^entry/(?P<const>tt\d{7})/$', views.entry_details, name='entry_details'),
+
     url(r'^year/$', views.entry_groupby_year, name='entry_groupby_year'),
     url(r'^year/(?P<year>\d{4})/$', views.entry_show_from_year, name='entry_show_from_year'),
 ]
