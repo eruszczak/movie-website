d70a16f18850184885bb5d38d661d791acc5824e
eryk <me@eryk.io>
2016-07-13 09:27:22 +0200

    view for entry details

--- mysite/movie/views.py
+++ mysite/movie/views.py
@@ -1,24 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db import connections
 
-def my_custom_sql():
-    last_ratings = "SELECT name, my_rate, imdb_rate, votes FROM Movie ORDER BY created DESC LIMIT 10"
-    # cursor = connections['ratings'].cursor()    # may cause an error
-    with connections['ratings'].cursor() as cursor:
-        cursor.execute(last_ratings)
-        return cursor
+from .models import Entry
+# def my_custom_sql():
+#     last_ratings = "SELECT name, my_rate, imdb_rate, votes FROM Movie ORDER BY created DESC LIMIT 10"
+#     cursor = connections['ratings'].cursor()    # may cause an error
+#     cursor.execute(last_ratings)
+#     return cursor
+    # with connections['ratings'].cursor() as cursor:
     # cursor.execute('select name, my_rate, imdb_rate, votes from movie where type_id=8')
     # row = cursor.fetchall()
     # return row
 
-# def dictfetchall(cursor):
-#     "Return all rows from a cursor as a dict"
-#     columns = [col[0] for col in cursor.description]
-#     return [
-#         dict(zip(columns, row))
-#         for row in cursor.fetchall()
-#     ]
 
 def index(request):
-    return render(request, 'movie/movie.html', {'lists': my_custom_sql()})
-    # return render(requests, 'movie/movie.html', {'list_dicts': dictfetchall(my_custom_sql())})
+    context = {
+        'last_rating': Entry.objects.all(),
+    }
+    return render(request, 'movie/entry.html', context)
+
+
+def entry_details(request, pk):
+    context = {
+        'entry': get_object_or_404(Entry, pk=pk),
+    }
+    return render(request, 'movie/entry_details.html', context)
