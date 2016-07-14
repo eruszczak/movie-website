b14c5932fb37cf6dd5002f60ed9138f5e0e71118
eryk <kasumierka@gmail.com>
2016-07-14 22:11:26 +0200

    added views for genres and put them in templates

--- mysite/movie/views.py
+++ mysite/movie/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.db import connections
 
-from .models import Entry
+from .models import Entry, Genre
 
 
 def index(request):
@@ -17,6 +17,7 @@ def about(request):
 def entry_details(request, const):
     context = {
         'entry': get_object_or_404(Entry, const=const),
+        # 'genres': get_object_or_404(Genre, const=const),
     }
 
     return render(request, 'movie/entry_details.html', context)
@@ -34,4 +35,16 @@ def entry_show_from_year(request, year):
     context = {
         'year': Entry.objects.order_by().filter(year=year)
     }
-    return render(request, 'movie/entry_from_year.html', context)
+    return render(request, 'movie/entry_show_from_year.html', context)
+
+def entry_groupby_genre(request):
+    context = {
+        'genre': Genre.objects.all()
+    }
+    return render(request, 'movie/entry_groupby_genre.html', context)
+
+def entry_show_from_genre(request, genre):
+    context = {
+        'genre': Genre.objects.get(name=genre).entry_set.all()
+    }
+    return render(request, 'movie/entry_show_from_genre.html', context)
