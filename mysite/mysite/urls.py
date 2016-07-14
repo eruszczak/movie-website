52b77b5e31dcad5eaa2f9fbd9667f2e9a542c5fb
eryk <me@eryk.io>
2016-07-14 18:22:56 +0200

    display images on details page, downloading posters

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -5,3 +5,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('movie.urls')),
 ]
+
+# from django.conf import settings
+# from django.conf.urls.static import static
+# if settings.DEBUG:
+    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
