e3454ed0310c519d6afa34ec2b5e29f07a9e338e
eryk <me@eryk.io>
2016-07-15 20:32:19 +0200

    added basic log system, requirements and back to sqlite for test deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,16 +77,16 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # },
     'default': {
-        'NAME': 'app_data',
-        'ENGINE': 'django.db.backends.postgresql',
-        'USER': 'postgres',
-        'PASSWORD': passwd2,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     },
+    # 'default': {
+    #     'NAME': 'app_data',
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'USER': 'postgres',
+    #     'PASSWORD': passwd2,
+    # },
     # 'ratings': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': os.path.join(BASE_DIR, 'ratings.sqlite'),
