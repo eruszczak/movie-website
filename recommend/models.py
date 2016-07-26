8bb01f1f8f6cfc9c07db65d05c08d885624ce52d
eryk <me@eryk.io>
2016-07-26 11:06:11 +0200

    fixed form validation, info to about page, message to form

--- recommend/models.py
+++ recommend/models.py
@@ -2,10 +2,9 @@ from django.db import models
 
 
 class Recommendation(models.Model):
-    const = models.CharField('IMDb URL or ID', max_length=50, unique=True)
+    const = models.CharField('title', max_length=50, unique=True)
     nick = models.CharField(max_length=30)
     note = models.CharField(blank=True, null=True, max_length=120)
     name = models.TextField(blank=True, null=True)
-    date = models.DateField(auto_now=False, auto_now_add=True)
-
-
+    date = models.DateTimeField(auto_now=False, auto_now_add=True)
+    date_insert = models.DateTimeField(auto_now=False, auto_now_add=True)
