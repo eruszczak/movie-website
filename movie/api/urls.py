2112e2a8065819415851dc8069e67a57951830f6
eryk <kasumierka@gmail.com>
2016-08-18 10:56:52 +0200

    genre view api

--- movie/api/urls.py
+++ movie/api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     url(r'^$', views.EntryListView.as_view(), name='entry_list'),
     url(r'^detail/(?P<slug>[\w-]+)/$', views.EntryDetailView.as_view(), name='entry_detail'),
     url(r'^genre/$', views.GenreListView.as_view(), name='genre_list'),
+    url(r'^g/$', views.Genre2.as_view(), name='genre_list'),
     url(r'^rated/$', views.RateListView.as_view(), name='rate_list'),
     url(r'^year/$', views.YearListView.as_view(), name='year_list'),
     url(r'^month/$', views.MonthListView.as_view(), name='month_list'),
