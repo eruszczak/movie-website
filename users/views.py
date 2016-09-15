bedb72f900a1cda3f5b2fb89a2b6ea2984e99643
eryk <me@eryk.io>
2016-09-15 13:10:54 +0200

    Better login, register

--- users/views.py
+++ users/views.py
@@ -1,42 +1,47 @@
 from django.shortcuts import render, redirect
 from django.contrib import auth, messages
-from .forms import UserForm
+from .forms import RegisterForm, LoginForm
+from django.core.urlresolvers import reverse
 
 
 def register(request):
-    if request.method == 'POST':
-        user_form = UserForm(data=request.POST)
-        if user_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-    else:
-        user_form = UserForm()
-    context = {
-        'form': user_form,
-    }
-    return render(request, 'users/register.html', context)
+    form_data = request.POST if request.method == 'POST' else None
+    form = RegisterForm(form_data)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+
+        created_user = auth.authenticate(username=user.username, password=password)
+        auth.login(request, created_user)
+        messages.success(request, 'Successful registration. You have been logged in, ' + user.username,
+                         extra_tags='alert-success')
+        return redirect(reverse('home'))
+    return render(request, 'users/register.html', {'form': form})
 
 
 def login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    form_data = request.POST if request.method == 'POST' else None
+    form = LoginForm(form_data)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
         user = auth.authenticate(username=username, password=password)
         if user:
             if user.is_active:
                 auth.login(request, user)
                 messages.success(request, 'You have been logged in: ' + user.username, extra_tags='alert-success')
-                return redirect('/')
+                return redirect(reverse('home'))
             else:
                 messages.error(request, 'User not active', extra_tags='alert-warning')
         else:
             messages.error(request, 'Invalid creditentials', extra_tags='alert-warning')
 
-    return render(request, 'users/login.html')
+    return render(request, 'users/login.html', {'form': form})
 
 
 def logout(request):
     auth.logout(request)
     messages.success(request, 'You have been logged out: ' + request.user.username, extra_tags='alert-success')
-    return redirect('/')
+    return redirect(request.META.get('HTTP_REFERER'))
