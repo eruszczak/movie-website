54dad8dd9d22a0266e8200d367ebfbddb49b9f11
eryk <kasumierka@gmail.com>
2016-09-15 13:10:54 +0200

    Better login, register

--- users/forms.py
+++ users/forms.py
@@ -2,9 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 
 
-class UserForm(forms.ModelForm):
+class RegisterForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ('username', 'password', 'email')
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
