182d971e9d3a609dfc1ab17f2fd77c7eeb8d9156
eryk <kasumierka@gmail.com>
2016-12-30 14:13:39 +0100

    home page changes

--- api/rating/urls.py
+++ api/rating/urls.py
@@ -8,14 +8,4 @@ urlpatterns = [
     url(r'^y/$', views.Years.as_view(), name='years'),
     url(r'^r/$', views.Rates.as_view(), name='rates'),
     url(r'^m/$', views.MonthlyRatings.as_view(), name='monthly'),
-    # url(r'^detail/(?P<slug>[\w-]+)/$', views.TitleDetailView.as_view(), name='title_detail'),
-    # url(r'^genre/$', views.GenreListView.as_view(), name='genre_list'),
-    # url(r'^genre/(?P<genre>\S+)/$', views.GenreDetailView.as_view(), name='genre_detail'),
-    # url(r'^g/$', views.Genre2.as_view(), name='genre_list'),
-    # url(r'^rated/$', views.RateListView.as_view(), name='rate_list'),
-    # url(r'^year/$', views.YearListView.as_view(), name='year_list'),
-    # url(r'^month/$', views.MonthListView.as_view(), name='month_list'),
-
-    # url(r'^update/(?P<slug>[\w-]+)/$', views.WatchAgainUpdateView.as_view(), name='watch_again'),
-    # url(r'^rated/(?P<rate>\d+)/$', views.RateListView.as_view(), name='rate_detail'),
 ]
