2de7cebd15cd5460755efdd1484fa08544894403
eryk <me@eryk.io>
2017-01-17 21:46:51 +0100

    backup file

--- common/backup.py
+++ common/backup.py
@@ -2,6 +2,32 @@ import django, os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
 django.setup()
 
-import json
+from django.core.management import call_command
 
-from movie.models import Title
+tables = ['movie.type', 'movie.director', 'movie.genre', 'movie.actor', 'movie.title']
+
+
+def backup_titles():
+    for table in tables:
+        filename = table + '.json'
+        with open(filename, 'w') as f:
+            call_command('dumpdata', table, stdout=f)
+
+
+def clear_titles():
+    from movie.models import Type, Director, Genre, Actor, Title
+    confirm = input('Do you want to delete tables?')
+    if confirm in ('yes', 'y'):
+        Title.objects.all().delete()
+        Type.objects.all().delete()
+        Director.objects.all().delete()
+        Genre.objects.all().delete()
+        Actor.objects.all().delete()
+
+
+def restore_titles():
+    for table in tables:
+        filename = table + '.json'
+        call_command('loaddata', filename)
+        # with open(filename, 'r') as f:
+        #     call_command('loaddata', table, stdin=f)
