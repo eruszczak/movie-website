16635f3fd222e4bf0454bc60eae9c8d56d9ef01b
eryk <kasumierka@gmail.com>
2017-05-24 11:53:37 +0200

    Add robots.txt

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
+    url(r'^robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     url(r'^', include('movie.urls')),
     url(r'^api/', include('api.urls')),
     url(r'^users/', include('users.urls')),
