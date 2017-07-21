31fee92334e62e0df4a4e3ac7f68ce1b87c788aa
eryk <kasumierka@gmail.com>
2017-07-21 20:55:59 +0200

    Add MessageMixin.

--- users/views_class.py
+++ users/views_class.py
@@ -1,16 +1,44 @@
 from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import redirect
+
+
+class MessageMixin:
+    # can check hasattr(self, 'request') and dialog. in __init__?
+    def set_success_message(self):
+        messages.warning(self.request, self.get_success_message())
+
+    def get_success_message(self):
+        return self.dialogs['success'].format(self.request.user.username)
+
+    def get_message_level(self):
+        raise NotImplementedError
 
 
 # permission - not logged
 # is_active is checked?
-class AuthLoginView(LoginView):
+class AuthLoginView(MessageMixin, LoginView):
     template_name = 'users/login.html'
+    extra_context = {
+        'title': 'Login Page'
+    }
 
     dialogs = {
-        'success': 'You have been logged in, '
+        'success': 'Welcome, {}.'
     }
 
     def get_success_url(self):
-        messages.warning(self.request, self.dialogs['success'].format(self.request.username))
-        return self.request.user.userprofile.get_absolute_url()
+        self.set_success_message()
+        return redirect(self.request.user.userprofile)
+
+
+class AuthLogoutView(MessageMixin, LogoutView):
+
+    dialogs = {
+        'success': 'You have been logged out, {}'
+    }
+
+    def get_next_page(self):
+        self.set_success_message()
+        return redirect(self.request.META.get('HTTP_REFERER'))
+
