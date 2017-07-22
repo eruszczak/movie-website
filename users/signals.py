6b8a92790e22b555d26cba3513cff77ff876102b
eryk <kasumierka@gmail.com>
2017-07-22 18:50:29 +0200

    Add signal for UserProfile creation and RegisterView.

---
+++ users/signals.py
@@ -0,0 +1,12 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import User, UserProfile
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """
+    create UserProfile for a new User
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
--- users/views_class.py
+++ users/views_class.py
@@ -1,23 +1,28 @@
 from django.contrib import messages
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from django.views.generic.edit import CreateView
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import never_cache
 from django.contrib.auth import logout
 
+from .forms import RegisterForm
+from .models import UserProfile
+
 
 class MessageMixin:
     # can check hasattr(self, 'request') and dialog. in __init__?
-    def set_success_message(self, attach_username=True):
+    def set_success_message(self, attach_username=True, username=None):
+        self.username = username or self.request.user.username
         msg = self.get_success_message_with_username() if attach_username else self.get_success_message()
         messages.warning(self.request, msg)
 
     def get_success_message_with_username(self):
-        return self.dialogs['success'].format(self.request.user.username)
+        return self.dialogs['success'].format(self.username)
 
     def get_success_message(self):
-        return self.dialogs['success'].format(self.request.user.username)
+        return self.dialogs['success']
 
     def get_message_level(self):
         raise NotImplementedError
@@ -28,7 +33,7 @@ class MessageMixin:
 class AuthLoginView(MessageMixin, LoginView):
     template_name = 'users/login.html'
     extra_context = {
-        'page_title': 'Login Page'
+        'page_title': 'Login'
     }
 
     dialogs = {
@@ -77,4 +82,28 @@ class AuthPasswordChangeView(MessageMixin, PasswordChangeView):
         return self.request.user.userprofile.get_absolute_url()
 
 
-# class AuthRegisterView(MessageMixin, )
+class AuthRegisterView(MessageMixin, CreateView):
+    template_name = 'users/register.html'
+    form_class = RegisterForm
+
+    dialogs = {
+        'success': 'Account created, {}. You are now logged in.'
+    }
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context.update({
+            'page_title': 'Register'
+        })
+        return context
+
+    def get_success_url(self):
+        """
+        when form_valid() calls form.save() new User is created and signal is emitted and receiver creates its Profile
+        so below I can query for that Profile and call its get_absolute_url so user is redirected to his profile
+        """
+        self.set_success_message(username=self.object.username)
+        return UserProfile.objects.get(user=self.object).get_absolute_url()
+
+    # def form_valid(self, form):
+    #     pass
