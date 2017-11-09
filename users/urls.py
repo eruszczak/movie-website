4c880b6c57322df98446a2e0408e9a95a4f205e0
eryk <me@eryk.io>
2017-11-09 20:40:16 +0100

    refactor url names, remove templates

--- users/urls.py
+++ users/urls.py
@@ -6,19 +6,17 @@ from . import views, views_auth
 
 urlpatterns = [
     url(r'^$', views.UserListView.as_view(), name='user-list'),
-
-    url(r'^login/$', views_auth.LoginView.as_view(), name='login-view'),
-    url(r'^logout/$', views_auth.LogoutView.as_view(), name='logout-view'),
-    url(r'^register/$', views_auth.RegisterView.as_view(), name='register-view'),
-    url(r'^password-change/$', views_auth.PasswordChangeView.as_view(), name='password-change-view'),
-
+    url(r'^login/$', views_auth.LoginView.as_view(), name='login'),
+    url(r'^logout/$', views_auth.LogoutView.as_view(), name='logout'),
+    url(r'^register/$', views_auth.RegisterView.as_view(), name='register'),
+    url(r'^password-change/$', views_auth.PasswordChangeView.as_view(), name='password-change'),
     url(r'^(?P<username>[-\w]+)/$', views.UserDetailView.as_view(), name='user-detail'),
     url(r'^(?P<username>[-\w]+)/edit/$', views.UserUpdateView.as_view(), name='user-edit'),
+
 
     url(r'^(?P<username>[-\w]+)/watchlist/$', movie.views.watchlist, name='watchlist'),
     url(r'^(?P<username>[-\w]+)/favourites/$', movie.views.favourite, name='favourite'),
     url(r'^(?P<username>[-\w]+)/recommend/$', recommend.views.recommend, name='recommend'),
-
     url(r'^(?P<username>[-\w]+)/import/$', views.import_ratings, name='import-ratings'),
     url(r'^(?P<username>[-\w]+)/export$', views.export_ratings, name='export-ratings'),
 ]
