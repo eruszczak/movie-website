d8c824e587cb1a8a0e58b2c357c754c99cf91714
eryk <kasumierka@gmail.com>
2017-11-09 01:11:40 +0100

    add pagination

--- movie/forms.py
+++ movie/forms.py
@@ -1,11 +1,14 @@
 from django import forms
+from django.db.models import Q, Count
 
-from movie.models import Title
+from movie.models import Title, Genre
 from movie.shared import SearchFormMixin
 
 
-class TitleSearchForm(SearchFormMixin, forms.ModelForm):
+class TitleSearchForm(SearchFormMixin, forms.Form):
+    genre = forms.ModelMultipleChoiceField(queryset=Genre.objects.annotate(num=Count('title')).order_by('-num'))
 
-    class Meta:
-        model = Title
-        fields = '__all__'
+    @staticmethod
+    def search_genre(value):
+        print(value)
+        return Q()
