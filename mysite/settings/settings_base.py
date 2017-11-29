e6eec714edd40213d6e1aaec5cc4affe5db2a94b
eryk <kasumierka@gmail.com>
2017-11-29 18:16:53 +0100

    change timezone, add new models

--- mysite/settings/settings_base.py
+++ mysite/settings/settings_base.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'mysite.urls'
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
