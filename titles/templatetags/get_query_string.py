a740d202ce9037ddbd7a27ca4c1e47f66b9be1be
eryk <me@eryk.io>
2017-11-29 11:39:50 +0100

    fix get_querystring tag and searching titles by type

---
+++ titles/templatetags/get_query_string.py
@@ -0,0 +1,25 @@
+from django import template
+from urllib.parse import urlencode, parse_qs, parse_qsl
+
+register = template.Library()
+
+
+@register.simple_tag(name='get_query_string')
+def get_query_string(get_request, *args):
+    """
+        returns encoded query string without page parameter and extra parameter names passed as *args
+        Using parse_qsl because I need a list and get_request.urlencode() because it gives an access to multiple values
+        eg. genre=2&genre=8
+    """
+    get_request = get_request.copy()
+    parameters_to_exclude = ['page']
+    parameters_to_exclude.extend(list(args))
+    for parameter in parameters_to_exclude:
+        try:
+            get_request.pop(parameter)
+        except KeyError:
+            pass
+
+    parameter_list = parse_qsl(get_request.urlencode())
+    encoded_query_string = urlencode(parameter_list)
+    return '&{}'.format(encoded_query_string) if encoded_query_string else ''
