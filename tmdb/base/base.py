e11f2a7d0edbe317e400f8f562aec2734a4f6c2e
eryk <me@eryk.io>
2017-12-03 13:31:11 +0100

    change tmdb folder structure, refactor find_by_id to use get_wrapper_class

--- tmdb/base/base.py
+++ tmdb/base/base.py
@@ -1,17 +1,11 @@
-import os
-import django
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-django.setup()
-
 import json
-
+import os
+from decouple import config
 from django.conf import settings
 
-from decouple import config
-
-from titles.constants import TITLE_CREW_JOB, MOVIE, SERIES
-from titles.models import Genre, Keyword, CastTitle, Person, CastCrew, Title, Season
 from shared.helpers import SlashDict, get_json_response
+from titles.constants import TITLE_CREW_JOB
+from titles.models import CastTitle, Person, Genre, CastCrew, Title, Keyword
 
 
 class TmdbResponseMixin:
@@ -167,88 +161,3 @@ class BaseTmdb(TmdbResponseMixin):
     def save_response_to_file(self, file_name):
         with open(self.source_file_path.format(file_name), 'w') as outfile:
             json.dump(self.api_response, outfile)
-
-
-class MovieTmdb(BaseTmdb):
-    title_type = MOVIE
-    imdb_id_path = 'imdb_id'
-
-    def __init__(self, *args, **kwargs):
-        # TODO: title_id in init? simpler
-        super().__init__(*args, **kwargs)
-        self.title_model_map.update({
-            'release_date': 'release_date',
-            'runtime': 'runtime',
-            'name': 'title',
-        })
-
-        self.response_handlers_map.update({
-            'keywords/keywords': self.save_keywords
-        })
-
-        self.urls['details'] = 'movie'
-
-
-class SeriesTmdb(BaseTmdb):
-    title_type = SERIES
-    imdb_id_path = 'external_ids/imdb_id'
-    seasons_model_map = {
-        'release_date': 'air_date',
-        'episodes': 'episode_count',
-        'number': 'season_number',
-        # "poster_path": "/xRTUb8oeQHGjyBWj7OOpkvUuvKO.jpg",
-    }
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.title_model_map.update({
-            'release_date': 'release_date',
-            'runtime': 'runtime',
-            'name': 'title',
-        })
-
-        self.response_handlers_map.update({
-            'keywords/results': self.save_keywords,
-            'seasons': self.save_seasons
-        })
-
-        self.urls['details'] = 'tv'
-
-    def save_seasons(self):
-        for season in self.api_response['seasons']:
-            season_data = {
-                attr_name: season[tmdb_attr_name] for attr_name, tmdb_attr_name in self.seasons_model_map.items()
-            }
-            Season.objects.create(title=self.title, **season_data)
-
-
-class Tmdb(TmdbResponseMixin):
-    """Based on imdb_id, returns either MovieTmdb or SeriesTmdb instance"""
-
-    def find_by_id(self, title_id):
-        """
-        I can either call /movie or /tv endpoint. When I have an title_id I don't know what type of title it is.
-        So I tried calling first endpoint and on failure called second - 2 requests at worst to get a response.
-        But the thing is, you can't call /tv with imdb_id - only tmdb_id. So I use `find` endpoint and it returns
-        whether an imdb_id is a movie/series and I know its tmdb_id, so I can call any endpoint.
-        """
-        try:
-            with open(self.source_file_path.format(title_id), 'r') as outfile:
-                response = json.load(outfile)
-                tmdb_pk = response['id']
-                if response['title_type'] == MOVIE:
-                    return MovieTmdb(tmdb_pk)
-                else:
-                    return SeriesTmdb(tmdb_pk)
-        except FileNotFoundError:
-            response = self.get_tmdb_response('find', str(title_id), qs={'external_source': 'imdb_id'})
-            if response is not None:
-                movie = response['movie_results']
-                if len(movie) == 1:
-                    tmdb_pk = movie[0]['id']
-                    return MovieTmdb(tmdb_pk)
-
-                series = response['tv_results']
-                if len(series) == 1:
-                    tmdb_pk = series[0]['id']
-                    return SeriesTmdb(tmdb_pk)
