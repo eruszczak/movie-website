c19e760de1c28c01ac3f5eeba12ec39e70004026
eryk <kasumierka@gmail.com>
2017-12-09 18:16:20 +0100

    save picture_path for persons, add picture fields to Poster model

--- shared/models.py
+++ shared/models.py
@@ -4,11 +4,11 @@ from os.path import join
 
 class FolderPathMixin:
     MODEL_FOLDER_NAME = None
-    INSTANCE_FOLDER_NAME = 'pk'
+    ATTRIBUTE_FOR_FOLDER_NAME = 'pk'
 
     def get_folder_path(self, absolute=False):
         """returns path of folder for model instance's files"""
-        relative_model_folder_path = join(self.MODEL_FOLDER_NAME, getattr(self, self.INSTANCE_FOLDER_NAME))
+        relative_model_folder_path = join(self.MODEL_FOLDER_NAME, getattr(self, self.ATTRIBUTE_FOR_FOLDER_NAME))
         if absolute:
             return join(settings.MEDIA_ROOT, relative_model_folder_path)
         return relative_model_folder_path
