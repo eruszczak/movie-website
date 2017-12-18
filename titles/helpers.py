47f2864fd91dac0382b62f4bbe884ffe8f1a40b6
eryk <kasumierka@gmail.com>
2017-12-18 19:14:24 +0100

    get person avatar from tmdb, display placeholders for slick lazy loaded items

--- titles/helpers.py
+++ titles/helpers.py
@@ -6,6 +6,9 @@ def tmdb_image(func):
 
     def func_wrapper(self):
         if settings.DEBUG:
+            if self.__class__.__name__ == 'Person':
+                # for developing I want to display different w185 placeholder for Person
+                return static('img/posters/w185_and_h278_bestv2_person.jpg')
             return static(f'img/posters/{func(self)}.jpg')
         if self.image_path:
             return f'http://image.tmdb.org/t/p/{func(self)}/{self.image_path}'
