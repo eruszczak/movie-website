f5d793b7f05ca423560b43202e3a40ae0ab0efd1
eryk <kasumierka@gmail.com>
2017-12-18 13:58:56 +0100

    choose poster source depending on DEBUG

--- titles/helpers.py
+++ titles/helpers.py
@@ -1,11 +1,11 @@
+from django.conf import settings
 from django.contrib.staticfiles.templatetags.staticfiles import static
 
 
 def tmdb_image(func):
-    local = True
 
     def func_wrapper(self):
-        if local:
+        if settings.DEBUG:
             return static(f'img/posters/{func(self)}.jpg')
         if self.image_path:
             return f'http://image.tmdb.org/t/p/{func(self)}/{self.image_path}'
