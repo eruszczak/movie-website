88770460dd01254e706693dd6e103b07dbca9ced
eryk <kasumierka@gmail.com>
2017-12-21 07:39:35 +0100

    add instance_required decorator and use it to refactor api views

--- titles/helpers.py
+++ titles/helpers.py
@@ -10,9 +10,22 @@ def tmdb_image(func):
                 # for developing I want to display different w185 placeholder for Person
                 return static('img/posters/w185_and_h278_bestv2_person.jpg')
             return static(f'img/posters/{func(self)}.jpg')
+
         if self.image_path:
             return f'http://image.tmdb.org/t/p/{func(self)}/{self.image_path}'
 
-        # raise Exception('Placeholder is needed')
+        # todo: raise Exception('Placeholder is needed')
+
+    return func_wrapper
+
+
+def instance_required(func):
+
+    def func_wrapper(self, request, *args, **kwargs):
+        message = self.set_instance(**kwargs)
+        if message:
+            return message
+
+        return func(self, request, *args, **kwargs)
 
     return func_wrapper
