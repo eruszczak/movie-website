a407223c36f4aa6a0296ab007536f9d303ae7484
eryk <kasumierka@gmail.com>
2017-12-22 13:36:13 +0100

    refactor folder management, create instance's folders only on demand

--- accounts/helpers.py
+++ accounts/helpers.py
@@ -47,16 +47,14 @@ def export_ratings(user):
     exports to a csv file all of user's ratings, so they can be imported later
     file consists of lines in format: tt1234567,2017-05-23,7
     """
-    user_tmp_folder = user.get_temp_folder_path(absolute=True)
+    user_tmp_folder = user.get_temp_folder_path(absolute=True, create=True)
     headers = ['imdb_id', 'rate_date', 'rate']
 
     ratings = Rating.objects.filter(user=user).select_related('title')
     count_ratings = ratings.count()
     count_titles = ratings.values_list('title').distinct().count()
-    filename = '{}_ratings_for_{}_titles_{}'.format(count_ratings, count_titles, now().strftime('%Y-%m-%d'))
 
-    # create random file_name in tmp
-    # todo: this file can already exist - what then
+    filename = '{}_ratings_for_{}_titles_{}'.format(count_ratings, count_titles, now().strftime('%Y-%m-%d'))
     file_path = join(user_tmp_folder, f'{filename}.csv')
     with open(file_path, 'w') as csvfile:
         writer = DictWriter(csvfile, fieldnames=headers, lineterminator='\n')
