e4092fb1c939b35a2538d1b84c4c0854e4cf4e0c
eryk <kasumierka@gmail.com>
2017-12-22 20:08:01 +0100

    fix header validation

--- common/prepareDB_utils.py
+++ common/prepareDB_utils.py
@@ -39,7 +39,7 @@ def convert_to_datetime(date_string, source):
         'imdb_csv': '%a %b %d 00:00:00 %Y',
         'my_csv': '%Y-%m-%d'
     }
-    if date_string and date_formats.get(source):
+    if date_formats.get(source):
         try:
             return datetime.strptime(date_string, date_formats[source])
         except ValueError:
@@ -55,6 +55,9 @@ def get_csv_headers(file):
     csv_reader = csv.reader(file)
     csv_headings = next(csv_reader)
     file.seek(0)
+    if len(csv_headings) == 1:
+        # imdb csv file uses only 1 column so all headers are in 1 column
+        csv_headings = csv_headings[0].replace('"', '').split(',')
     return csv_headings
 
 
@@ -68,9 +71,9 @@ def get_csv_headers(file):
 #     return get_csv_headers(file) == expected_headers
 
 
-def valid_csv_header(file, expected_headers):
-    """return True if all of expected headers are present in csv file's headers"""
-    headers = get_csv_headers(file)
+def valid_csv_header(headers, expected_headers):
+    """return True if all of expected headers are present in headers"""
+    print([h in headers for h in expected_headers])
     return all(h in headers for h in expected_headers)
 
 
