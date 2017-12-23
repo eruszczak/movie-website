e34feb6a34ec2331ce0325534ec8dc6c700604ec
eryk <me@eryk.io>
2017-12-23 12:24:18 +0100

    refactor user-edit url

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
     url(r'^register/$', views.RegisterView.as_view(), name='register'),
     # url(r'^password-change/$', views_auth.PasswordChangeView.as_view(), name='password-change'),
+    url(r'^settings/$', views.UserUpdateView.as_view(), name='user-edit'),
     url(r'^(?P<username>[-\w]+)/$', views.UserDetailView.as_view(), name='user-detail'),
-    url(r'^(?P<username>[-\w]+)/edit/$', views.UserUpdateView.as_view(), name='user-edit'),
     # url(r'^(?P<username>[-\w]+)/recommend/$', recommend_views.recommend, name='recommend'),
 ]
