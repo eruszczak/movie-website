e0da52f6c81ffde3b1d01deddbba57ea974d0262
eryk <me@eryk.io>
2017-12-24 19:07:13 +0100

    User model cleanup

--- accounts/models.py
+++ accounts/models.py
@@ -10,13 +10,11 @@ from titles.models import Title, Rating
 
 
 class User(FolderPathMixin, AbstractUser):
+    update_date = models.DateTimeField(auto_now=True)
+
     picture = models.ImageField(upload_to=get_random_file_path, blank=True, null=True)
     imdb_id = models.CharField(blank=True, max_length=15)
     tagline = models.CharField(blank=True, max_length=100)
-    last_updated_csv_ratings = models.DateTimeField(null=True, blank=True)
-    last_updated_rss_ratings = models.DateTimeField(null=True, blank=True)
-    last_updated_rss_watchlist = models.DateTimeField(null=True, blank=True)
-    last_updated_profile = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     MODEL_FOLDER_NAME = 'accounts'
 
@@ -87,32 +85,6 @@ class User(FolderPathMixin, AbstractUser):
     def count_titles(self):
         """counts rated distinct titles"""
         return Title.objects.filter(rating__user=self).distinct().count()
-
-    @property
-    def can_update_csv_ratings(self):
-        return self.have_minutes_passed(self.last_updated_csv_ratings)
-
-    @property
-    def can_update_rss_ratings(self):
-        return self.have_minutes_passed(self.last_updated_rss_ratings)
-
-    @property
-    def can_update_rss_watchlist(self):
-        return self.have_minutes_passed(self.last_updated_rss_watchlist)
-
-    @staticmethod
-    def have_minutes_passed(time):
-        if time:
-            return (timezone.now() - time).seconds > 180
-        return False
-
-    @staticmethod
-    def get_extension_condition(file, what_to_delete):
-        if what_to_delete == 'picture':
-            return any(file.endswith(ext) for ext in ['.jpg', '.png'])
-        elif what_to_delete == 'csv':
-            return file.endswith('.csv')
-        return False
 
 
 class UserFollow(models.Model):
