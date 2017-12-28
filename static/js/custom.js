658954eae59e6dcaa2b3dab5f1eb17637d5447df
eryk <me@eryk.io>
2017-12-28 21:25:37 +0100

    fix badly inited slick when tab becomes visible

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,33 +1,23 @@
 $('[type="tooltip"]').popup();
 $('.ui.checkbox').checkbox();
-
 $('.tabular.menu .item').tab();
 $('.ui.menu').tab();
 $('.ui.dropdown, .dropdown.item').dropdown();
-
-$('.title-menu .item').tab();
 $('.ui.embed').embed();
+$('.ui.accordion').accordion();
 
-
-$('.recommend.dropdown').dropdown({
-    onChange: function(value, text, choice) {
-        var selectedOptions = value.split(',');
-        if (selectedOptions.length > 0 && selectedOptions[0].length > 0) {
-            $('.recommend.button').removeClass('hide');
-        } else {
-            $('.recommend.button').addClass('hide');
-        }
+$('.title-menu .item').tab({
+    onVisible: function(tabPath, parameterArray, historyEvent) {
+        $('.slick-carousel-small').slick('setPosition');
     }
 });
 
-// every image I want to lazy load is rendered with `data-src` and `type="src"`
+// every image  I want to lazy load is rendered with `data-src` and `type="src"`
 $('[type="src"]').visibility({
     type: 'image',
     transition: 'fade in',
     duration: 500
 });
-
-$('.ui.accordion').accordion();
 
 $('.clear.tiny.modal').modal('attach events', '.clear-ratings.button', 'show');
 $('.export.tiny.modal').modal('attach events', '.export-ratings.button', 'show');
