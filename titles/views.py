9e6169e5d12997976c144d3203cf7ee90cce71f4
eryk <me@eryk.io>
2017-12-30 18:12:56 +0100

    display rate_date in title_card,

--- titles/views.py
+++ titles/views.py
@@ -66,21 +66,9 @@ class TitleListView(TitleSearchMixin):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        len_of_get = len(self.request.GET)
-        only_user_ratings = self.searched_user and len_of_get == 1
-
-        # todo: this will be not needed anymore
-        only_pagination_and_ratings = self.request.GET.get('page') and len_of_get == 2 and self.searched_user
-
-        # hide search box if:
-        # - looking for a user's ratings (or paginated ratings)
-        # - there is no GET parameters
-        show_search_box = (len_of_get > 1 or (len_of_get > 0 and not only_user_ratings))\
-                          and not only_pagination_and_ratings
 
         context.update({
             # 'searched_user': self.searched_user,
-            'show_search_box': show_search_box,
             # 'owner_looks_at_his_ratings': self.searched_user and self.request.user and self.request.user.pk == self.searched_user.pk
         })
         return context
