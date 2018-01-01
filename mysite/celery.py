199c3584f95575782f8a7e0827bd202dd37890fd
eryk <me@eryk.io>
2018-01-01 20:25:26 +0100

    add periodic task

--- mysite/celery.py
+++ mysite/celery.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
+from titles.tasks import task_run_daily_tmdb_tasks
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 app = Celery('mysite', backend='amqp', broker='amqp://')
@@ -12,6 +15,14 @@ app = Celery('mysite', backend='amqp', broker='amqp://')
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+@app.on_after_configure.connect
+def setup_periodic_tasks(sender, **kwargs):
+    sender.add_periodic_task(
+        crontab(hour=21, minute=1),
+        task_run_daily_tmdb_tasks.s()
+    )
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
