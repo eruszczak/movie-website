0a65aa7761d00b000702c5789640ff6da3101d1d
Eryk <me@eryk.io>
2022-08-18 20:27:07 +0200

    Clean up

--- src/mysite/settings/settings_production.py
+++ src/mysite/settings/settings_production.py
@@ -24,22 +24,9 @@ LOGGING = {
             'level': 'DEBUG',
             'class': 'logging.StreamHandler',
         }
-#         'logfile': {
-#             'level':'DEBUG',
-#             # 'class':'logging.FileHandler',
-#             'class': 'logging.handlers.RotatingFileHandler',
-#             'filename': LOGS_ROOT,
-#             'maxBytes': 1024 * 1024 * 20,
-#         },
     },
     'root': {
         'level': 'INFO',
         'handlers': ['console']
     },
 }
-
-if 'HTTPS' in os.environ:
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
